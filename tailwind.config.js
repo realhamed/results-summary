65005ca55d53b26342a5c0cae7732f789cd05584
realhamed <91184501+realhamed@users.noreply.github.com>
2023-12-21 18:21:37 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        newRed: {
+          500: "#ff5757",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
